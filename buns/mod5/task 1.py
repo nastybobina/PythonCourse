2e57b684c78d59cbc79c0c1f15282baa9fc7c0a8
catsumi_ha <98974466+nastybobina@users.noreply.github.com>
2023-10-29 21:44:56 +0500

    Add files via upload

--- buns/mod5/task 1.py
+++ buns/mod5/task 1.py
@@ -2,22 +2,36 @@ class EmptyStackException(Exception):
     pass
 
 class Stack:
-
+    """
+    Основной класс для стека
+    """
     class Node:
+        """
+        Вспомогательный класс для узлов списка
+        """
         def __init__(self, data, next_elem):
             self.top_elem_data = data
             self.next_elem = next_elem
 
     def __init__(self):
-        self.top_elem = None
+        self.top_elem = None  # ссылка на конец стека
 
     def elemtIsEmpty(self):
+        """
+        проверка стека на пустоту
+        """
         return self.top_elem is None
 
     def push(self, data):
+        """
+        добавление элемента val в конец списка
+        """
         self.top_elem = Stack.Node(data, self.top_elem)
 
     def pop(self):
+        """
+        возвращение последнего элемента из списка с удалением его из списка
+        """
         if self.elemtIsEmpty():
             current_elem = self.top_elem.top_elem_data
             self.top_elem = self.top_elem.next_elem
@@ -26,6 +40,9 @@ class Stack:
             raise EmptyStackException
 
     def peek(self):
+        """
+        вывод на печать содержимого стека
+        """
         if not self.elemtIsEmpty():
             return self.top_elem.top_elem_data
         else:
